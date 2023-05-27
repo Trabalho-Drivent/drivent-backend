0120c814313a196f95ab9c8a232971f9622b7fb9
Denilson Lima <denilsondslima@gmail.com>
2023-05-26 23:51:42 -0300

    fix: expected format

--- src/repositories/activities-repository/index.ts
+++ src/repositories/activities-repository/index.ts
@@ -2,14 +2,57 @@
 import { prisma } from '@/config';
 
 async function getActivitiesByParams(date: string) {
-  return prisma.activitiesSchedule.findMany({
+  const activities = await prisma.activities.findMany({
     where: {
-      startsAt: {
-        gte: new Date(`${date}T00:00:00Z`),
-        lt: new Date(`${date}T23:59:59Z`),
+      ActivitiesSchedule: {
+        startsAt: {
+          gte: new Date(`${date}T00:00:00Z`),
+          lt: new Date(`${date}T23:59:59Z`),
+        },
+      },
+    },
+    include: {
+      ActivitiesSchedule: {
+        include: {
+          ActivitiesLocal: true,
+        },
       },
     },
   });
+
+  // Objeto para agrupar as atividades por local
+  const activitiesByLocal: Record<number, { id: number; name: string; activities: any[] }> = {};
+
+  activities.forEach((activity) => {
+    const {
+      ActivitiesSchedule,
+      ActivitiesSchedule: { ActivitiesLocal },
+    } = activity;
+
+    if (ActivitiesLocal) {
+      const { id, name } = ActivitiesLocal;
+
+      if (!activitiesByLocal[id]) {
+        activitiesByLocal[id] = {
+          id,
+          name,
+          activities: [],
+        };
+      }
+
+      activitiesByLocal[id].activities.push({
+        id: activity.id,
+        name: activity.name,
+        startsAt: ActivitiesSchedule.startsAt,
+        endsAt: ActivitiesSchedule.endsAt,
+      });
+    }
+  });
+
+  // Converter o objeto em um array de objetos
+  const result = Object.values(activitiesByLocal);
+
+  return result;
 }
 
 const activitieRepository = {
